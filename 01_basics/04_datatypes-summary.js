e072e702627e0572d8cfdc1f33ed176a23df50da
1999chandan <chanvishwakarma@gmail.com>
2024-04-07 11:42:53 +0530

    stack (Primitive), Heap (non primitive)

--- 01_basics/04_datatypes-summary.js
+++ 01_basics/04_datatypes-summary.js
@@ -28,3 +28,35 @@ const firstObj = {
 } // values inside {} are object stored in key value pair and type or value can by any type
 
 //https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ //Memory type:
+
+// stack (Primitive), Heap (non primitive)
+
+// example of "Stack memory":--
+let oneName = "Star Light Photography"; // oneName value is assigned to a stack let say stack-1
+
+let anotherName = oneName; // in another name we assigned value of onName so in this case anotherName does not 
+                           // took the original value of oneName, It took copy of oneName value and saved in other stack let say stack-2
+anotherName = "Vasu_v7"; // here when we are changing value of anotherName so it will change value of stack-2 not stack-1, so oneName will not change only anotherName value will change.
+
+console.log(oneName); // output "Star Light Photography"
+console.log(anotherName);  // output "Vasu_v7"
+
+// Example of "Heap":--
+
+let userOne = {
+    email:"chandan@gmail.com",
+    name: "chandan",
+    age: 24
+}; // value of userOne stored in Heap-1
+
+let userTwo = userOne // in this case it will not create a copy and store in other memory location.
+                      // here it will take value from original location that is "Heap-1".
+//So if we change value of userTwo it will change value in "Heap-1" because of that it will change the value of userOne as well.
+
+userTwo.email = "chan@gmail.com";
+
+console.log(userOne.email); // output: "chan@gmail.com"
+console.log(userTwo.email); // output: "chan@gmail.com"
